9c15044c201dd6b6ee50f625fc9d77b4a4e65c7a
Jesus Yanquen <jdyanquenc@udistrital.edu.co>
2025-09-13 12:43:39 -0500

    v0.0.0.2 Cargar mapa

---
+++ docs/program.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([4.628063362852504, -74.06589315072523], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
